523fbf930f91a930b700fa01acbacc764171596b
forestbr <arthursilcorrea@gmail.com>
2023-06-24 20:06:32 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+
+var vel = 60.5
+console.log('a velocidade do seu carro e ' + vel +' km/h')
+if (vel > 60) {
+    console.log('voce foi multado pro passar a velocidade de 60 km/h')
+}
+console.log('dirija sempre com o cinto de segurança! ')
