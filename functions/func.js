6ed6c4b4695f47d36bfc2c19871f9c46144bd92e
arthur sil <50556704+arthur-sil@users.noreply.github.com>
2023-08-06 23:59:07 -0300

    ip

---
+++ functions/func.js
@@ -0,0 +1,14 @@
+function variavel() {
+    let num= 10, num2 = 20, soma = num +num2
+    return soma}
+
+console.log(variavel())
+
+//anonima
+let anonimaf = function () {
+    return 'sou o retorno da funcao'
+}
+console.log(anonimaf())
+// arrow functions
+let arrowf = () => 'sou o retorno na arrow function'
+console.log(arrowf())
